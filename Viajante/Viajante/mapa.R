eca8eb738a7dbb54b6e244dda6e392f4107fac01
Daniel Bernardes Pozzan <39958473+pozzandan@users.noreply.github.com>
2022-01-12 21:05:38 -0300

    Update mapa.R

--- Viajante/Viajante/mapa.R
+++ Viajante/Viajante/mapa.R
@@ -26,27 +26,27 @@ while (i < num_gen) {
   
   dataSP <- dados[arg1:arg2, ]
 
-aux1 <- dataSP$Longitude[-1]
-xend_cities <- append(aux1, dataSP$Longitude[1])
+  aux1 <- dataSP$Longitude[-1]
+  xend_cities <- append(aux1, dataSP$Longitude[1])
 
-aux2 <- dataSP$Latitude[-1]
-yend_cities <- append(aux2, dataSP$Latitude[1])
+  aux2 <- dataSP$Latitude[-1]
+  yend_cities <- append(aux2, dataSP$Latitude[1])
 
 
-citiesSP <- data.frame(gen = dataSP$Geracao,
-                       lon = dataSP$Longitude, 
-                       lat = dataSP$Latitude, 
-                       muni = dataSP$Nome)
+  citiesSP <- data.frame(gen = dataSP$Geracao,
+                         lon = dataSP$Longitude, 
+                         lat = dataSP$Latitude, 
+                         muni = dataSP$Nome)
 
-leg <- paste("Geração: ", dataSP$Geracao[1])
-subleg <- paste("Distância: ", dataSP$Distancia[1], "km")
+  leg <- paste("Geração: ", dataSP$Geracao[1])
+  subleg <- paste("Distância: ", dataSP$Distancia[1], "km")
 
-print(ggplot(sp)+
-  geom_sf(fill = '#FDF9E4')+
-  geom_point(data = citiesSP, aes(x = lon, y = lat), size = 2, pch = 21)+
-  geom_segment(data = citiesSP, aes(x = lon, y = lat, xend = xend_cities, 
+  print(ggplot(sp)+
+    geom_sf(fill = '#FDF9E4')+
+    geom_point(data = citiesSP, aes(x = lon, y = lat), size = 2, pch = 21)+
+    geom_segment(data = citiesSP, aes(x = lon, y = lat, xend = xend_cities, 
                                     yend = yend_cities),color = "blue", 
-               arrow = arrow(length = unit(0.2,"cm")))+
+                                   arrow = arrow(length = unit(0.2,"cm")))+
   theme_light()+labs(title = leg, subtitle = subleg, x = NULL, y = NULL))
 
   i <- i+1
