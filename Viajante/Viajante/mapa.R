2101dc1ace5349e848f1b8eec7a5ab1235e90887
Daniel Bernardes Pozzan <39958473+pozzandan@users.noreply.github.com>
2022-01-13 09:02:12 -0300

    Update mapa.R

--- Viajante/Viajante/mapa.R
+++ Viajante/Viajante/mapa.R
@@ -1,10 +1,13 @@
+#Bibliotecas necessárias
 library(ggplot2)
 library(geobr)
 library(ggrepel)
 library(animation)
 
+#Definição manual do número de cidades a serem analisadas
 n <- 40
 
+#Leitura e adequação dos dados (saída do programa principal)
 dados <- read.csv('caminhos.csv', sep = ";", row.names = NULL)
 colnames(dados) <- colnames(dados)[2:ncol(dados)]
 dados <- dados[ , - ncol(dados)]  
@@ -12,42 +15,51 @@ dados$Latitude <- scan(text=dados$Latitude, dec=",", sep=".")
 dados$Longitude <-scan(text=dados$Longitude, dec=",", sep=".")
 dados$Geracao <- as.integer(dados$Geracao)
 
+#Número de gerações
 num_gen <- nrow(dados)/n
 
-sp <- read_state(code_state = "SP")
+#Leitura dos dados geográficos
+state <- read_state(code_state = "SP")
 options(ggrepel.max.overlaps = Inf)
 
+#Loop de geração dos gráficos
 i <- 0
 saveGIF({
-while (i < num_gen) {
-  
-  arg1 <- i*n+1
-  arg2 <- n*(i+1)
-  
-  dataSP <- dados[arg1:arg2, ]
-
-  aux1 <- dataSP$Longitude[-1]
-  xend_cities <- append(aux1, dataSP$Longitude[1])
-
-  aux2 <- dataSP$Latitude[-1]
-  yend_cities <- append(aux2, dataSP$Latitude[1])
-
-
-  citiesSP <- data.frame(gen = dataSP$Geracao,
-                         lon = dataSP$Longitude, 
-                         lat = dataSP$Latitude, 
-                         muni = dataSP$Nome)
-
-  leg <- paste("Geração: ", dataSP$Geracao[1])
-  subleg <- paste("Distância: ", dataSP$Distancia[1], "km")
-
-  print(ggplot(sp)+
-    geom_sf(fill = '#FDF9E4')+
-    geom_point(data = citiesSP, aes(x = lon, y = lat), size = 2, pch = 21)+
-    geom_segment(data = citiesSP, aes(x = lon, y = lat, xend = xend_cities, 
-                                    yend = yend_cities),color = "blue", 
-                                   arrow = arrow(length = unit(0.2,"cm")))+
-  theme_light()+labs(title = leg, subtitle = subleg, x = NULL, y = NULL))
-
-  i <- i+1
-}})
+  while (i < num_gen) {
+    
+    #Para leitura das linhas do arquivo de n em n (1 a n, n+1 a 2n, ..., (num_gen-1)n+1, num_gen*n))
+    arg1 <- i*n+1
+    arg2 <- n*(i+1)
+    
+    data_state <- dados[arg1:arg2, ]
+    
+    #Vetores de fim para segmentos - vetores lat e long shiftados por um
+    aux1 <- data_state$Longitude[-1]
+    xend_cities <- append(aux1, data_state$Longitude[1])
+    
+    aux2 <- data_state$Latitude[-1]
+    yend_cities <- append(aux2, data_state$Latitude[1])
+    
+    #Data frame para plotagem
+    cities_state <- data.frame(gen = data_state$Geracao,
+                               lon = data_state$Longitude, 
+                               lat = data_state$Latitude, 
+                               muni = data_state$Nome)
+    
+    #Legendas
+    leg <- paste("Geração: ", data_state$Geracao[1])
+    subleg <- paste("Distância: ", data_state$Distancia[1], "km\nCidade de saída: ",
+                    cities_state[1, ]$muni)
+    
+    #Plotagem dos gráficos para geração do GIF final
+    print(ggplot(state)+
+            geom_sf(fill = '#FDF9E4')+
+            geom_point(data = cities_state, aes(x = lon, y = lat), size = 2)+
+            geom_label_repel(size = 2.5, data = cities_state[1, ], aes(x = lon, y = lat, label = muni))+
+            geom_segment(data = cities_state, aes(x = lon, y = lat, xend = xend_cities, 
+                                                  yend = yend_cities),color = "blue", 
+                         arrow = arrow(length = unit(0.2,"cm")))+
+            theme_light()+labs(title = leg, subtitle = subleg, x = NULL, y = NULL))
+    
+    i <- i+1
+  }}, movie.name = 'evolução.gif', interval = 0.5)
